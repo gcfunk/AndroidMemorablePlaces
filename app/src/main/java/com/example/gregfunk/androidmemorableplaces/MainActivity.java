f39f074a12e762d4f7d07da171f8ef8ca45aa169
Greg Funk <greg.funk@walltowall.com>
2015-11-27 14:10:13 -0500

    Adds list view to main activity

--- app/src/main/java/com/example/gregfunk/androidmemorableplaces/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androidmemorableplaces/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.gregfunk.androidmemorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> places = new ArrayList<>();
+
+        places.add("Add a new place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
